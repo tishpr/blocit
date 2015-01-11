e2069d9bf395af254db69e6c3c1e929d8ca54e36
[tishpr] <[tishsny@gmail.com]>
2015-01-11 09:59:55 -0800

    removed :name from add user to post file

--- db/migrate/20150102232802_add_user_to_posts.rb
+++ db/migrate/20150102232802_add_user_to_posts.rb
@@ -1,7 +1,7 @@
 class AddUserToPosts < ActiveRecord::Migration
   def change
     add_column :posts, :user_id, :integer
-    add_index :posts, :user_id, :name # create an index on user_id by adding
+    add_index :posts, :user_id, #:name # create an index on user_id by adding
     # an add_index line in the migration's change method:
     #Indexes help optimize queries with associated attributes,
     # as is the case with querying the posts table for a specific user_id
