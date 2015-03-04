2c4ca734d4e856bb2110132fd2e85705e724563d
[tishpr] <[tishsny@gmail.com]>
2015-03-03 18:18:46 -0800

    down_vote

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -4,18 +4,30 @@
  include  Devise::TestHelpers
 
  describe VotesController do
- 
- 
-   describe '#up_vote' do
-     it "adds an up-vote to the post" do
-      request.env["HTTP_REFERER"] = '/'
+  
+ before do 
+       request.env["HTTP_REFERER"] = '/'
        @user = authenticated_user
        @post = associated_post
        sign_in @user
+ end
  
+   describe '#up_vote' do
+     it "adds an up-vote to the post" do
        expect {
          post( :up_vote, post_id: @post.id )
        }.to change{ @post.up_votes }.by 1
      end
    end
+
+  describe '#down_vote' do
+     it "adds a down-vote to the post" do
+       expect {
+         post( :down_vote, post_id: @post.id )
+       }.to change{ @post.down_votes }.by 1
+     end
+   end
+
+
+
  end
