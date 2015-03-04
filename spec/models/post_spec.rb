4d8e4405623e381b03b28dd50ae91fc292d4cdcd
[tishpr] <[tishsny@gmail.com]>
2015-03-03 18:05:46 -0800

    votes-controller spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,7 @@
  require 'rails_helper'
+ include TestFactories
+
+
  
  describe Post do
 
@@ -37,28 +40,6 @@
    end
 
 
-        def associated_post(options={})
-
-          post_options = {
-              title: 'Post title',
-              body: 'Post bodies must be pretty long.',
-              topic: Topic.create(name: 'Topic name'),
-              user: authenticated_user
-                          }.merge(options)
-
-          Post.create(post_options)
-      end
-
-       def authenticated_user(options={})
-         user_options = {email: "email#{rand}@fake.com", password: 'password'}.merge(options)
-         user = User.new(user_options)
-         user.skip_confirmation!
-         user.save
-         user
-       end
- end
-#end
-
-    
-
- end
+       
+   end  
+end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,30 +1,33 @@
 
 require 'rails_helper'
+include TestFactories
 
-#RSpec.
-describe Vote, type: :model do
-  
+describe Vote do
   describe "validations" do
-
-    describe "value validation" 
-      
-      before do
-        @up_vote = Vote.new(value: 1)
-        @down_vote = Vote.new(value: -1)
-        @invalid_vote = Vote.new(value: 2)
-      end
-
+    describe "value validation" do
       it "only allows -1 or 1 as values" do
-       
-      expect( @up_vote.valid? ).to eq( true )
-      expect( @down_vote.valid? ).to eq( true )
-      expect( @invalid_vote.valid? ).to eq( false )
+        up_vote = Vote.new(value: 1)
+        expect(up_vote.valid?).to eq(true)
+
+        down_vote = Vote.new(value: -1)
+        expect(down_vote.valid?).to eq(true)
+
+        invalid_vote = Vote.new(value: 2)
+        expect(invalid_vote.valid?).to eq(false)
       end
- 
     end
+  end
+
+   describe 'after_save' do
+     it "calls `Post#update_rank` after save" do
+       post = associated_post
+       vote = Vote.new(value: 1, post: post)
+       expect(post).to receive(:update_rank)
+       vote.save
+     end
+   end
 
 end
-
 
 
 
