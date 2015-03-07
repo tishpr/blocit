0c3f412b188b22fb326c1ba45c0ef2f314463a34
[tishpr] <[tishsny@gmail.com]>
2015-03-07 08:45:28 -0800

    fix premature error message

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,61 +1,55 @@
 class PostsController < ApplicationController
  #  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]:destroy
-#def create
-    #@article = Article.find(params[:article_id])
-    # ...
-  #end
-    def show
-      @topic = Topic.find(params[:topic_id])
-      @post = Post.find(params[:id]) 
-      @comments = @post.comments
 
-    end
+      def show
+        @topic = Topic.find(params[:topic_id])
+        @post = Post.find(params[:id]) 
+        @comments = @post.comments
+
+      end
 
       def new   # instance variable assigned to the return of Post.new related view: posts#new
         @topic = Topic.find(params[:topic_id])
         @post = Post.new
          authorize @post
-      end
+       end
 
-                  def create
-                          @topic = Topic.find(params[:topic_id])
-                          @post = current_user.posts.build(post_params)  
-                          @post.topic = @topic   
-                          authorize @post
-                          @post.transaction do
-                          #@post.save_with_initial_vote!(value: 1)
-                          flash[:notice] = "Post was saved."
-                          redirect_to [@topic, @post]     # expect the user to return to the show view of the Post they just created.
-                        end
-                     
-                       flash[:error] = "Snap! There was an error saving the post. Please try again."
-                       #render :new
-                     end
-                  end
+      def create
+        @topic = Topic.find(params[:topic_id])
+        @post = current_user.posts.build(post_params)  
+        @post.topic = @topic   
+        authorize @post
+        @post.save_with_initial_vote
+        if @post.votes(:value == 1) # This fixed the problem of error msg in wrong place
+         flash[:notice] = "Post was saved."
+         redirect_to [@topic, @post]     # expect the user to return to the show view of the Post they just created.
+           
+          else
+           flash[:error] = "Snap! There was an error saving the post. Please try again."
+           render :new
+           end
+      end
               
 
-
-
-        def edit
-          @topic = Topic.find(params[:topic_id])
-          @post = Post.find(params[:id])  #defines object
-          authorize @post   # authorizing object
-        end
+      def edit
+        @topic = Topic.find(params[:topic_id])
+        @post = Post.find(params[:id])  #defines object
+        authorize @post   # authorizing object
+      end
 
     def update
-      @topic = Topic.find(params[:topic_id])
-      @post = Post.find(params[:id])
-      authorize @post  
+        @topic = Topic.find(params[:topic_id])
+        @post = Post.find(params[:id])
+        authorize @post  
 
-          if @post.update_attributes(post_params)
-           flash[:notice] = "Post was updated."
-           redirect_to [@topic, @post]
+        if @post.update_attributes(post_params)
+         flash[:notice] = "Post was updated."
+         redirect_to [@topic, @post]
 
-       else
-         flash[:error] = "Snap! There was an error saving the post. Please try again."
-         render  :edit
-       end
-       
+           else
+             flash[:error] = "Snap! There was an error saving the post. Please try again."
+             render  :edit
+           end
      end
 
               def destroy
@@ -72,12 +66,13 @@ class PostsController < ApplicationController
                    render :show
                  end
               end
-   
 
-   private
-    def post_params
-      params.require(:post).permit(:title, :body, :image)
-    end
+
+
+             private
+              def post_params
+                params.require(:post).permit(:title, :body, :image)
+              end
    
-#end
+end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,53 +1,52 @@
+
+
 class Post < ActiveRecord::Base
 
-    has_many :comments, dependent: :destroy
-    has_many :votes, dependent: :destroy
-    belongs_to :user
-    belongs_to :topic                           # associates post to Topics
-    mount_uploader :image, ImageUploader
-    #after_create :create_vote
-
-  def up_votes  
-    votes.where(value: 1).count
-  end
-
-  def down_votes 
-    votes.where(value: -1).count
-  end
-
-  def points 
-    votes.sum(:value)
-  end
-
-   def update_rank
-     age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
-     new_rank = points + age_in_days
-     update_attribute(:rank, new_rank)
-   end
-      # default_scope { order('created_at DESC') } ~ orders from most recent first ~
-   default_scope { order('rank DESC') } # ordered by rank
-
-   validates :topic, presence: true
-   validates :user, presence: true
-   validates :title, length: { minimum: 5 }, presence: true
-   validates :body, length: { minimum: 20 }, presence: true
- 
- private
-  def create_vote
-    #user.
-    user.votes.create({value: 1, post: self})
-    #Vote.create(value: 1, post_id: self.id, user_id: user.id)
-  end
-
-  #def save_with_initial_vote! do  |t|
-    #  @post = Post.new 
-    #  @post.user.create_vote(1, self)
-    #  @post.save
-
-  #end
+            has_many :comments, dependent: :destroy
+            has_many :votes, dependent: :destroy
+            belongs_to :user
+            belongs_to :topic                           # associates post to Topics
+            mount_uploader :image, ImageUploader
+            
+           default_scope { order('rank DESC') } # ordered by rank
+           validates :topic, presence: true
+           validates :user, presence: true
+           validates :title, length: { minimum: 5 }, presence: true
+           validates :body, length: { minimum: 20 }, presence: true
 
 
+
+          def up_votes  
+            votes.where(value: 1).count
+          end
+
+          def down_votes 
+            votes.where(value: -1).count
+          end
+
+          def points 
+            votes.sum(:value)
+          end
+
+           def update_rank
+             age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
+             new_rank = points + age_in_days
+             update_attribute(:rank, new_rank)
+           end
+
+           def save_with_initial_vote
+            Post.transaction do 
+              self.create_vote
+              #self.save
+              end
+           end
+           
+           #private
+            def create_vote
+              #user.
+              user.votes.create({value: 1, post: self})
+              
+            end
+
+           
 end
-# this is a Post class model   app/models/post.rb
-# it goes along with a migration file (begins with a timestamp)
-# db/migrate/20141231timeinminhours_create_posts.rb 
