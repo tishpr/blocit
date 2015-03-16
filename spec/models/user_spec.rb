9f5c83a4ec1150d0b4d9ee64f973faf45bf66ded
[tishpr] <[tishsny@gmail.com]>
2015-03-16 16:06:53 -0700

    Complete favoriting assignment

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,29 @@ require 'rails_helper'
    include TestFactories
  
    describe "#favorited(post)" do
-     xit "returns `nil` if the user has not favorited the post" do
-     end
- 
-     xit "returns the appropriate favorite if it exists" do
-     end
+    before do
+      @another_not_favorite_post = associated_post
+      @favorite_post = associated_post
+      @user = authenticated_user
+      @favorite = Favorite.create(post_id: @favorite_post.id, user_id: @user.id)
+      @not_favorite = associated_post
+
+    end
+
+    it "returns `nil` if the user has not favorited the post" do
+      expect( @user.favorited(@not_favorite) ).to eq(nil)
+    end
+
+    it "returns the appropriate favorite if it exists" do
+      expect( @user.favorited(@favorite_post)).to eq(@favorite)
+    end
+
+    it "returns `nil` if the user has favorited another post" do
+      expect( @user.favorited(@another_not_favorite_post)).to eq(nil)
+    end
+
+
    end
  end
+
+ # creating common objects in a before block inside of describe "#favorited(post)"
