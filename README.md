25e9b3f112c42c10dc3683668d681a1144336dab
[tishpr] <[tishsny@gmail.com]>
2015-03-17 21:23:36 -0700

    more edits on t-s

--- README.md
+++ README.md
@@ -15,26 +15,34 @@ Things you may want to cover:
 > created db in checkpoint models? 
 
 * Database initialization
-> db initialization ? think i did it.
+> db initialization 
 
 * How to run the test suite
       http://sourceforge.net/projects/zaproxy/files/2.3.1/ZAP_2.3.1_Lite.tar.gz/download
+
+
+
+
 
 * Services (job queues, cache servers, search engines, etc.)
 
 * Deployment instructions
 
 * ...
+
+
 > Using a “password meter” to the right of input field give 
 > a highly visual way to gauge the quality of the answer.  
 >    
-> This meter seems to compel people to answer the question...
+>>This meter seems to compel people to answer the question...
 
 Mint.com registration form interrupts people when they are answering questions with immediate error messages.
 ## Bloccit: a Reddit clone to teach the fundamentals of web development and Rails 4.
 
 * Made with my mentor at [Bloc](http://bloc.io).
 
+
+* notes: Callbacks allow you to trigger logic before or after an alteration of an object's state.
 
 ###  Heroku at: tish-bloccit.herokuapp.com [tish-bloccit](http://tish-bloccit.herokuapp.com/ "Tisha's version").
 
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -2,41 +2,37 @@ describe Topic do
    describe "scopes" do
  
      before do 
-       @public_topic = Topic.create # default is public
-       @private_topic = Topic.create(public: false)
+       @public_topic = Topic.create(name: 'Sandy', public: true) # default is public
+       @private_topic = Topic.create(name: 'Jimmy', public: false)
 
      end
  
      describe "publicly_viewable" do
        it "returns a relation of all public topics" do
          expect(Topic.publicly_viewable).to eq( [@public_topic] )
-         scope :publicly_viewable -> where(public: true) }
-      
-     end
+       end
      end
  
      describe "privately_viewable" do
        it "returns a relation of all private topics" do
-        expect(Topic.publicly_viewable).to eq( [@private_topic] )
-         scope :privately_viewable -> { where(public: false) }          
+        expect(Topic.privately_viewable).to eq( [@private_topic] )
+        expect(user).where(current_user)
+         scope :privately_viewable, -> (current_user: true) where(public: 'false')           
        end
      end
  
      describe "visible_to(user)" do
        it "returns all topics if the user is present" do
          user = true # sneaky solution; we don't need a real user, just something truthy
-         expect(Topic.publicly_viewable), public: true) 
-         scope :visible_to, -> {where(user: true, public: true)}
+         expect(Topic.publicly_viewable).to eq(public: true)  
+         scope :visible_to, -> (user) { where(public: true) }
          end   # Your code here
        end
  
        it "returns only public topics if user is nil" do
         user = nil
         expect(Topic.publicly_viewable).where(user: nil)
-      end
-        user = nil
-         @public_topic.publicly_viewable.where(user)      # Your code here
-       end
+      #end
      end
    end
  end
