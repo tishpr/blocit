9c71b6197830b05655079e95040f94298b4f7e7b
[tishpr] <[tishsny@gmail.com]>
2015-03-25 20:13:48 -0700

    added email pref

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,19 +8,16 @@ class Comment < ActiveRecord::Base
    private
  
    def send_favorite_emails
-     post.favorites.each do |favorite|
-       FavoriteMailer.new_comment(favorite.user, post, self).deliver
-     end
-   end
 
-   after_create :send_favorite_emails
- 
-   private
- 
-   def send_favorite_emails
      post.favorites.each do |favorite|
-       FavoriteMailer.new_comment(favorite.user, post, self).deliver
+      if should_receive_update_for?(favorite)
+         FavoriteMailer.new_comment(favorite.user, self.post, self).deliver
+       end
      end
+
    end
-  
+ 
+   def should_receive_update_for?(favorite)
+     user_id != favorite.user_id && favorite.user.email_favorites?
+   end  
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -12,6 +12,10 @@
        @comment = Comment.new(body: 'My comment', post: @post, user_id: 10000)
      end
  
+      # We don't need to change anything for this condition;
+     # The email_favorites attribute defaults to true
+     context "with user's permission" do
+
      it "sends an email to users who have favorited the post" do
        @user.favorites.where(post: @post).create
  
@@ -22,12 +26,20 @@
  
        @comment.save
      end
+   end
  
-     it "does not send emails to users who haven't" do
-       expect( FavoriteMailer )
-         .not_to receive(:new_comment)
+  context "without permission" do
  
-       @comment.save
+       before { @user.update_attribute(:email_favorites, false) }
+ 
+       it "does not send emails, even to users who have favorited" do
+         @user.favorites.where(post: @post).create
+ 
+         expect( FavoriteMailer )
+           .not_to receive(:new_comment)
+ 
+         @comment.save
+       end
      end
    end
  end
