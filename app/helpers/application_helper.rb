75ab690099c8485aa0a3cc0d6a33144a886a2d58
[tishpr] <[tishsny@gmail.com]>
2015-03-26 21:48:12 -0700

    another-interlude-assignment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,36 +1,35 @@
 module ApplicationHelper
 
     def my_name
-      p "Tish  "
+     "Tish  "
     end
 
-       def form_group_tag(errors, &block)
-           if errors.any?
-             content_tag :div, capture(&block), class: 'form-group has-error'
-           else
-             content_tag :div, capture(&block), class: 'form-group'
-           end
+    def form_group_tag(errors, &block)
+       if errors.any?
+         content_tag :div, capture(&block), class: 'form-group has-error'
+       else
+         content_tag :div, capture(&block), class: 'form-group'
        end
+    end
 
-        def markdown(text)
-            renderer = Redcarpet::Render::HTML.new
-            extensions = {fenced_code_blocks: true}
-            redcarpet = Redcarpet::Markdown.new(renderer, extensions)
-            (redcarpet.render text).html_safe
-        end
+    def markdown(text)
+        renderer = Redcarpet::Render::HTML.new
+        extensions = {fenced_code_blocks: true}
+        redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+        (redcarpet.render text).html_safe
+    end
 
+     def up_vote_link_classes(post)
+      (current_user.voted(post) && current_user.voted(post).up_vote?) ? 'voted' : '' 
+    end
 
-
-
-
-
-
-
-
-
-
-
-
+     def down_vote_link_classes(post)
+        if (current_user.voted(post) && current_user.voted(post).down_vote?) 
+          'voted' 
+        else  
+            ''
+        end 
+    end
 
  end
   
