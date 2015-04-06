51738e5046f7e7da7141db1389232a9841e17fdc
[tishpr] <[tishsny@gmail.com]>
2015-04-05 17:26:35 -0700

    Complete public profiles assignment

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -3,13 +3,11 @@
 describe "Visiting profiles" do
 
   include TestFactories
+  include  Warden::Test::Helpers
+            Warden.test_mode!
 
   before do
-    @user = authenticated_user
-     @post = associated_post(user: @user)
-      @comment = Comment.new(user: @user, body: "A Comment")
-    allow(@comment).to receive(:send_favorite_emails)
-    @comment.save
+    comment_without_email
   end
 
   describe "not signed in" do
@@ -22,6 +20,24 @@ describe "Visiting profiles" do
        expect( page ).to have_content(@post.title)
        expect( page ).to have_content(@comment.body)
     end
-
   end
+
+  before do
+    comment_without_email
+    login_as(@user, :scope => :user)
+  end
+
+  describe "signed in" do
+
+    it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+
+       expect( page ).to have_content(@user.name)
+       expect( page ).to have_content(@post.title)
+       expect( page ).to have_content(@comment.body)
+    end
+  end
+
+
 end
--- spec/support/test_factories.rb
+++ spec/support/test_factories.rb
@@ -2,23 +2,30 @@ module TestFactories
 
  def associated_post(options={})
 
-          post_options = {
-              title: 'Post title',
-              body: 'Post bodies must be pretty long.',
-              topic: Topic.create(name: 'Topic name'),
-              user: authenticated_user
-                          }.merge(options)
+  post_options = {
+      title: 'Post title',
+      body: 'Post bodies must be pretty long.',
+      topic: Topic.create(name: 'Topic name'),
+      user: authenticated_user
+                  }.merge(options)
 
-          Post.create(post_options)
-      end
+   Post.create(post_options)
+  end
 
-       def authenticated_user(options={})
-         user_options = {email: "email#{rand}@fake.com", password: 'password'}.merge(options)
-         user = User.new(user_options)
-         user.skip_confirmation!
-         user.save
-         user
-       end
+ def authenticated_user(options={})
+   user_options = {email: "email#{rand}@fake.com", password: 'password'}.merge(options)
+   user = User.new(user_options)
+   user.skip_confirmation!
+   user.save
+   user
+ end
 
+ def comment_without_email
+   @user = authenticated_user
+   @post = associated_post(user: @user)
+   @comment = Comment.new(user: @user, body: "A Comment", post: @post)
+    allow(@comment).to receive(:send_favorite_emails)
+   @comment.save
+ end
 
 end
